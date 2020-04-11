d41171273b1ee050ead0fcf3f2476ec47a31e530
ChrisBPSU <32135165+ChrisBPSU@users.noreply.github.com>
2020-04-11 17:37:50 -0400

    Removed the writerOut and writerErr, writers are now attached to the severity Replace the Singleton format with global unexported variables

--- settings.go
+++ settings.go
@@ -2,8 +2,6 @@ package carte
 
 import (
 	"errors"
-	"io"
-	"os"
 	"sync"
 	"time"
 )
@@ -12,82 +10,43 @@ import (
 var (
 	ErrNegativeFunctionNameLength = errors.New("received a negative function name length")
 	ErrLocationWasNil             = errors.New("received a nil time.Location")
-	ErrWriterWasNil               = errors.New("received a nil io.Writer")
 )
 
-type settings struct {
-	functionNameLength int
-	location           *time.Location
-	dateFormat         string
-
-	writerOut io.Writer
-	writerErr io.Writer
+// Settings
+var (
+	functionNameLength = 16
+	timezone           = time.UTC
+	dateFormat         = "2006-01-02T15:04:05 MST"
 
 	mux sync.Mutex
-}
-
-// Settings is a singleton
-var (
-	Settings = &settings{
-		functionNameLength: 16,
-		location:           time.UTC,
-		dateFormat:         "2006-01-02T15:04:05 MST",
-		writerOut:          os.Stdout,
-		writerErr:          os.Stderr,
-		mux:                sync.Mutex{},
-	}
 )
 
-func (s *settings) SetFunctionNameLength(length int) error {
-	if length < 0 {
+func SetFunctionNameLength(fnl int) error {
+	if fnl < 0 {
 		return ErrNegativeFunctionNameLength
 	}
 
-	s.mux.Lock()
-	s.functionNameLength = length
-	s.mux.Unlock()
+	mux.Lock()
+	functionNameLength = fnl
+	mux.Unlock()
 
 	return nil
 }
 
-func (s *settings) SetLocation(loc *time.Location) error {
-	if loc == nil {
+func SetTimezone(tz *time.Location) error {
+	if tz == nil {
 		return ErrLocationWasNil
 	}
 
-	s.mux.Lock()
-	s.location = loc
-	s.mux.Unlock()
+	mux.Lock()
+	timezone = tz
+	mux.Unlock()
 
 	return nil
 }
 
-func (s *settings) SetDateFormat(dateFormat string) {
-	s.mux.Lock()
-	s.dateFormat = dateFormat
-	s.mux.Unlock()
-}
-
-func (s *settings) SetWriterOut(writerOut io.Writer) error {
-	if writerOut == nil {
-		return ErrWriterWasNil
-	}
-
-	s.mux.Lock()
-	s.writerOut = writerOut
-	s.mux.Unlock()
-
-	return nil
-}
-
-func (s *settings) SetWriterErr(writerErr io.Writer) error {
-	if writerErr == nil {
-		return ErrWriterWasNil
-	}
-
-	s.mux.Lock()
-	s.writerErr = writerErr
-	s.mux.Unlock()
-
-	return nil
+func SetDateFormat(format string) {
+	mux.Lock()
+	dateFormat = format
+	mux.Unlock()
 }
