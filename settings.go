24272597202f71474bf0a92cbabbdb8fd6592cf9
ChrisBPSU <32135165+ChrisBPSU@users.noreply.github.com>
2020-04-13 12:17:46 -0400

    Function name length is -1 by default, and returns the entire function name Setting function name length no longer returns an error

--- settings.go
+++ settings.go
@@ -8,29 +8,23 @@ import (
 
 // Errors
 var (
-	ErrNegativeFunctionNameLength = errors.New("received a negative function name length")
-	ErrLocationWasNil             = errors.New("received a nil time.Location")
+	ErrLocationWasNil = errors.New("received a nil time.Location")
 )
 
 // Settings
 var (
-	functionNameLength = 16
+	// -1 denotes the entire length
+	functionNameLength = -1
 	timezone           = time.UTC
 	dateFormat         = "2006-01-02T15:04:05 MST"
 
 	mux sync.Mutex
 )
 
-func SetFunctionNameLength(fnl int) error {
-	if fnl < 0 {
-		return ErrNegativeFunctionNameLength
-	}
-
+func SetFunctionNameLength(fnl int) {
 	mux.Lock()
 	functionNameLength = fnl
 	mux.Unlock()
-
-	return nil
 }
 
 func SetTimezone(tz *time.Location) error {
