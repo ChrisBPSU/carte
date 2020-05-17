0a69f233ce87af1290c4aec80f7a659bdb1ca145
ChrisBPSU <32135165+ChrisBPSU@users.noreply.github.com>
2020-05-16 21:01:15 -0400

    Replaced reflection with Json only parameters Moved Log to be a method on the severity struct

--- carte.go
+++ carte.go
@@ -1,38 +1,26 @@
 package carte
 
 import (
-	"encoding/json"
-	"fmt"
-	"reflect"
+	"strconv"
 	"strings"
+	"time"
 )
 
-type Jsonable interface {
-	Json() (name string, value string)
-}
-
-// Log writes the writer specified in the severity struct
-func Log(sev *severity, message string, details ...interface{}) {
-	_, _ = log(sev, message, details...)
-}
-
-// Rlog is the same as log, but returns the values from the call to Write()
-func Rlog(sev *severity, message string, details ...interface{}) (int, error) {
-	return log(sev, message, details...)
-}
-
 // Made the log one line to allow for a quick synchronous write to a custom writer with mutex locking
-func log(sev *severity, message string, details ...interface{}) (int, error) {
-	// Get date
-	date := getDate()
+func log(sev *severity, details ...Jsonable) (int, error) {
+	w := sev.GetWriter()
+	// Ignore nil writers
+	if w == nil {
+		return 0, nil
+	}
 
 	// Get func name
-	funcName := getFuncName()
+	funcName := getCaller()
 
 	// Rough estimate of all the required wrappers to a log, NOT the info
 	// TODO: Cant efficiently calculate the size of the final log, find another way to save allocations
-	// Reduces the number of allocations
-	// Base of 42 + (if len dtls > 0) -> 9 + len dtls * 6
+	// Can most likely calculate it now, will need to reorder
+	// Reduces the number of allocations, essentially just a guess since the latest patches
 	baseLogLen := 42
 	if len(details) > 0 {
 		baseLogLen += 9
@@ -41,66 +29,46 @@ func log(sev *severity, message string, details ...interface{}) (int, error) {
 	jsonLog := make([]byte, 0, baseLogLen)
 
 	// DATE
-	jsonLog = append(jsonLog, `{"Time":"`...)
-	jsonLog = append(jsonLog, date...)
+	jsonLog = append(jsonLog, `{"TS":`...)
+	jsonLog = append(jsonLog, strconv.FormatInt(time.Now().Unix(), 10)...)
 
 	// FUNC
 	if funcName != nil {
-		jsonLog = append(jsonLog, `","Func":"`...)
+		jsonLog = append(jsonLog, `,"FN":"`...)
 		jsonLog = append(jsonLog, funcName...)
+		jsonLog = append(jsonLog, '"')
 	}
 
 	// TYPE
-	jsonLog = append(jsonLog, `","Severity":"`...)
+	jsonLog = append(jsonLog, `,"SV":"`...)
 	jsonLog = append(jsonLog, sev.name...)
-
-	// MESSAGE
-	jsonLog = append(jsonLog, `","Message":"`...)
-	jsonLog = append(jsonLog, message...)
 	jsonLog = append(jsonLog, '"')
 
 	// DETAILS
-	if len(details) > 0 {
-		jsonLog = append(jsonLog, `,"Details":[`...)
-		for i, dtl := range details {
-			if dtl == nil {
-				continue
-			}
-
-			// Conversions get more expensive further down the type switch
-			// Jsonable is the fastest and clearest in expressing information
-			var name, value string
-			switch v := dtl.(type) {
-			case Jsonable:
-				name, value = v.Json()
-			case fmt.Stringer:
-				name = reflect.TypeOf(dtl).String()
-				value = v.String()
-			case error:
-				name = reflect.TypeOf(dtl).String()
-				value = strings.ReplaceAll(v.Error(), "\"", "'")
-			default:
-				name = reflect.TypeOf(dtl).String()
-				if val, err := json.Marshal(v); err != nil {
-					value = strings.ReplaceAll(err.Error(), "\"", "'")
-				} else {
-					value = strings.ReplaceAll(string(val), "\"", "\\\"")
-				}
-			}
-
-			jsonLog = append(jsonLog, '"')
-			jsonLog = append(jsonLog, name...)
-			jsonLog = append(jsonLog, `":"`...)
-			jsonLog = append(jsonLog, value...)
-			jsonLog = append(jsonLog, '"')
-
-			// If this is not the last detail append a comma
-			if i != len(details)-1 {
-				jsonLog = append(jsonLog, ","...)
-			}
+	for i, dtl := range details {
+		if dtl == nil {
+			continue
 		}
-		jsonLog = append(jsonLog, ']')
+
+		name, val := dtl.Json()
+		name = strings.ReplaceAll(name, "\"", "'")
+		name = strings.ReplaceAll(name, "\n", "")
+		val = strings.ReplaceAll(val, "\"", "'")
+		val = strings.ReplaceAll(val, "\n", "")
+
+		jsonLog = append(jsonLog, ",\""...)
+		jsonLog = append(jsonLog, name...)
+		jsonLog = append(jsonLog, `":"`...)
+		jsonLog = append(jsonLog, val...)
+		jsonLog = append(jsonLog, '"')
+
+		// If this is not the last detail append a comma
+		if i != len(details)-1 {
+		}
 	}
+
+	// Newlines break json parsing
+	jsonLog = []byte(strings.ReplaceAll(string(jsonLog), "\n", ""))
 	jsonLog = append(jsonLog, "}\n"...)
 
 	if hook := sev.getHook(); hook != nil {
@@ -110,5 +78,10 @@ func log(sev *severity, message string, details ...interface{}) (int, error) {
 	// TODO: add ability to write to StdOut as well if the writer is not
 	// Probably in settings
 
-	return sev.GetWriter().Write(jsonLog)
+	n, err := w.Write(jsonLog)
+	// Panic
+	if sev == Panic {
+		panic("carte.Panic called")
+	}
+	return n, err
 }
